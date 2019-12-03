02e151cb045b2a24296d7019d4297f2f914a16c5
Whittney Davis <wcdavis89@yahoo.com>
2019-12-03 02:14:38 +0000

    Automatically backed up by Learn

--- spec/06_patient_spec.rb
+++ spec/06_patient_spec.rb
@@ -50,7 +50,7 @@ describe 'Patient' do
       doctor_who = Doctor.new('The Doctor')
       doctor_what = Doctor.new('Das Doktor')
       hevydevy = Patient.new('Devin Townsend')
-      hevydevy.new_appointment(doctor_who, 'Friday, January 32nd', )
+      hevydevy.new_appointment( 'Friday, January 32nd', doctor_who)
       hevydevy.new_appointment( 'Saturday, January 32nd', doctor_what)
 
       expect(hevydevy.doctors).to include(doctor_who)
